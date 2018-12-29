235616dd80d9818c3302f0cbd29bd3255d9aeb5f
Sendhuraan NKK <sendhuraan967@gmail.com>
2018-12-29 21:50:13 +0530

    Added reverse

--- src/collection/reverse-bits/basic/index.js
+++ src/collection/reverse-bits/basic/index.js
@@ -1,5 +1,12 @@
 (function() {
 
-	
+	var reverseBits = require('./reverse-bits');
+
+	var input = 65536;
+
+	//console.log(input.toString(2));
+	console.log(reverseBits.reverseBits_swap(input).toString(2));
+	//console.log(reverseBits.reverseBits_swap(input));
+	console.log(reverseBits.reverseBits_lookupTable(input).toString(2));
 	
 })();
--- src/collection/reverse-bits/basic/reverse-bits.js
+++ src/collection/reverse-bits/basic/reverse-bits.js
@@ -1,5 +1,60 @@
 (function() {
 
-	
+	var BitUtils = require('@sendhuraan/js-utilities').BitUtils;
+
+	function reverseBits_swap(x, totalBits = 16) {
+
+		//console.log(x.toString(2));
+
+		var startBit = 1;
+		var endBit = totalBits + 1;
+
+		for(bitPositionA = startBit, bitPositionB = endBit; bitPositionA < bitPositionB; bitPositionA++, bitPositionB--) {
+
+			x = BitUtils.swapBits(x, bitPositionA, bitPositionB);
+			//console.log(x.toString(2));
+		}
+
+		return x;
+		
+	}
+
+	// TODO: lookupTable function is not working. Need to fix.
+	function reverseBits_lookupTable(x) {
+
+		function buildTable() {
+			var table = [];
+
+			for(var i = 0; i < 2**16; i++) {
+				table[i] = reverseBits_swap(i);
+			}
+
+			return table;
+		}
+
+		var precomputedReverse = buildTable();
+
+		function reverseBits() {
+			var WORD_SIZE = 8;
+			var BITMASK = 0xFF;
+
+			return (
+				precomputedReverse[x & BITMASK] << (3 * WORD_SIZE) |
+				precomputedReverse[(x >> WORD_SIZE) & BITMASK] << (2 * WORD_SIZE) |
+				precomputedReverse[(x >> (2 * WORD_SIZE)) & BITMASK] << WORD_SIZE |
+				precomputedReverse[(x >> (3 * WORD_SIZE)) & BITMASK]
+			);
+		}
+
+		return reverseBits();
+		
+	}
+
+	var publicAPI = {
+		reverseBits_swap: reverseBits_swap,
+		reverseBits_lookupTable: reverseBits_lookupTable
+	}
+
+	module.exports = publicAPI;
 	
 })();
