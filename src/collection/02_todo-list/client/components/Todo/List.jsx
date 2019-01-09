956c9171534736906120f64e530d9e0eb2014ade
Sendhuraan NKK <sendhuraan967@gmail.com>
2019-01-10 00:06:02 +0530

    Modified list.jsx for code communication

--- src/collection/02_todo-list/client/components/Todo/List.jsx
+++ src/collection/02_todo-list/client/components/Todo/List.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 
-const List = props => (
-  <ul>
-    {props.items.map((item, key) => (
-      <li key={key} className={`${item.completed ? 'completed' : 'pending'}`}>
-        {item.task}
+const List = function(props) {
+	return (
+		<ul>
+			{
+				props.items.map(function(item, key) {
 
-        <div className="actions">
-          <span className={item.completed ? 'hide' : 'done'} onClick={() => props.markAsCompleted(item.id)}>
-            <i className="fa fa-check"></i>
-          </span>
+					return (
+						<li key={key} className={`${item.completed ? 'completed' : 'pending'}`}>
+        				{item.task}
 
-          <span className="trash" onClick={() => props.removeTask(item.id)}>
-            <i className="fa fa-trash"></i>
-          </span>
-        </div>
-      </li>
-    ))}
-  </ul>
-);
+						<div className='actions'>
+							<span className={item.completed ? 'hide' : 'done'} 
+								onClick={() => props.markAsCompleted(item.id)}>
+								<i className='fa fa-check'></i>
+							</span>
+
+							<span className='trash' 
+								onClick={() => props.removeTask(item.id)}>
+								<i className='fa fa-trash'></i>
+							</span>
+						</div>
+
+        				</li>
+					)
+				
+				})
+			}
+		</ul>
+	)
+}
 
 export default List;
