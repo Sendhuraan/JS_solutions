1241108358be36588e8080f6f0d8807b212e7332
Sendhuraan NKK <sendhuraan967@gmail.com>
2019-01-09 20:29:26 +0530

    Modified folder name & changes to html

---
+++ src/collection/01_simple-website/client/components/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Home from './Home/Home';
+import Header from './shared/layout/Header';
+import Content from './shared/layout/Content';
+import Footer from './shared/layout/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header title="Simple Website" />
+
+        <Content>
+          <Home />
+        </Content>
+        
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
