f4ad71e669a0185d784e71ae5ee1c050d0733b0e
Sendhuraan NKK <sendhuraan967@gmail.com>
2019-01-24 11:03:50 +0530

    cleanup in parity

--- src/collection/5.1_parity/basic/index.js
+++ src/collection/5.1_parity/basic/index.js
@@ -1,23 +1,12 @@
 (function() {
 
 	var parity = require('./parity');
-	var BitUtils = require('@sendhuraan/js-utilities').BitUtils;
 
-	var input = 4294967295;
+	var input = 0b1011101100;
 
-	var inputSwap = 47;
-
-	Number.prototype.toBinary = BitUtils.convertToBinary;
-
-	// console.log(BitUtils.countSetBits_shiftBits(input));
-	// console.log(BitUtils.countSetBits_clearSetBits(input));
-	// console.log(BitUtils.convertToBinary(input));
-	// console.log(inputSwap.toBinary());
-	console.log(BitUtils.swapBits(inputSwap, 2, 5));
-
-	// console.log(parity.parity_shiftBits(input));
-	// console.log(parity.parity_clearSetBits(input));
-	// console.log(parity.parity_lookupTable(input));
-	// console.log(parity.parity_xor(input));
+	console.log(parity.parity_shiftBits(input));
+	console.log(parity.parity_clearSetBits(input));
+	console.log(parity.parity_lookupTable(input));
+	console.log(parity.parity_xor(input));
 	
 })();
