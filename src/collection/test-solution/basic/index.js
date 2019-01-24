6313123042effb6a53f6a998c14942ea410dd78b
Sendhuraan NKK <sendhuraan967@gmail.com>
2019-01-24 16:47:36 +0530

    Sort method signature

--- src/collection/test-solution/basic/index.js
+++ src/collection/test-solution/basic/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+(function() {
+
+	var SortUtils = require('@sendhuraan/js-utilities').SortUtils;
+
+	var input = [6,3,8,9,3,5];
+
+	console.log(SortUtils.BubbleSort.improved(input));
+	
+})();
