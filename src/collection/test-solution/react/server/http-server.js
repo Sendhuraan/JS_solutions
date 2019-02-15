5d9a3fb3955f3463b54b8f047f1bac616dc54098
Sendhuraan NKK <sendhuraan967@gmail.com>
2019-02-15 19:24:34 +0530

    modified server file

--- src/collection/test-solution/react/server/http-server.js
+++ src/collection/test-solution/react/server/http-server.js
@@ -1,39 +1,3 @@
-// (function() {
-
-// 	var finalhandler = require('finalhandler');
-// 	var http = require('http');
-// 	var serveStatic = require('serve-static');
-// 	var util = require('util');
-
-
-// 	function run(host, port, dir) {
-
-// 		var serve = serveStatic(dir, {'index': ['index.html', 'index.htm']});
-
-// 		var server = http.createServer(function onRequest (req, res) {
-// 			serve(req, res, finalhandler(req, res));
-// 		});
-
-// 		server.listen(port, host, function(error) {
-
-// 			if(error) {
-// 				console.log(error);
-// 			}
-// 			else {
-// 				console.log('Server Listening on http://' + host + ':' + port);
-// 			}
-			
-// 		});
-		
-// 	}
-
-// 	module.exports = {
-// 		run: run
-// 	};
-
-// })();
-
-
 (function() {
 
 	var finalhandler = require('finalhandler');
