05687cf0e09875c0eeb0dd5cb8e9530bcce32126
Sendhuraan NKK <sendhuraan967@gmail.com>
2019-06-16 15:01:29 +0530

    improved code readability

--- src/collection/react-cookbook-examples/04_lifecycle-methods/02_pomodoro_componentDidMount/client/components/PomodoroTimer/index.jsx
+++ src/collection/react-cookbook-examples/04_lifecycle-methods/02_pomodoro_componentDidMount/client/components/PomodoroTimer/index.jsx
@@ -7,82 +7,146 @@ class PomodoroTimer extends Component {
 		super();
 
 		this.state = {
-			time: 0
+			time: 0,
+			alert: {
+				type: '',
+				message: ''
+			}
 		};
 
-		this.timeFor = {
-			work: 1500,
-			shortBreak: 300,
-			longBreak: 900
+		this.constants = {
+			work: {
+				time: 10,
+				alert: {
+					type: 'work',
+					message: 'Working'
+				}
+			},
+			shortBreak: {
+				time: 300,
+				alert: {
+					type: 'short-break',
+					message: 'Taking short break'
+				}
+			},
+			longBreak: {
+				time: 900,
+				alert: {
+					type: 'long-break',
+					message: 'Taking long break'
+				}
+			},
+			buzz: {
+				time: 0,
+				alert: {
+					type: 'buzz',
+					message: 'Buzzzzzz!'
+				}
+			}
 		};
 
-		this.setTimeForWork = this.setTimeForWork.bind(this);
-		this.setTimeForShortBreak = this.setTimeForShortBreak.bind(this);
-		this.setTimeForLongBreak = this.setTimeForLongBreak.bind(this);
+		this.setTimeAndMessage = this.setTimeAndMessage.bind(this);
+		this.startWork = this.startWork.bind(this);
+		this.takeShortBreak = this.takeShortBreak.bind(this);
+		this.takeLongBreak = this.takeLongBreak.bind(this);
+		this.formatTime = this.formatTime.bind(this);
+		this.startCountDown = this.startCountDown.bind(this);
+		this.stopOrResetCountDown = this.stopOrResetCountDown.bind(this);
 	}
 
 	componentDidMount() {
-		this.setTimeForWork();
+		const { time, alert } = this.constants.work;
+		const { type, message } = alert;
+
+		this.setTimeAndMessage(time, type, message);
 	}
 
-	setTimeForWork() {
+	setTimeAndMessage(time, alertType, alertMessage) {
 		this.setState({
-			time: this.timeFor.work
+			time: time,
+			alert: {
+				type: alertType,
+				message: alertMessage
+			}
 		});
+	}
 
+	startWork() {
+		const { time, alert } = this.constants.work;
+		const { type, message } = alert;
+
+		this.setTimeAndMessage(time, type, message);
 		this.startCountDown();
 	}
 
-	setTimeForShortBreak() {
-		this.setState({
-			time: this.timeFor.shortBreak
-		});
-
+	takeShortBreak() {
+		const { time, alert } = this.constants.shortBreak;
+		const { type, message } = alert;
+		
+		this.setTimeAndMessage(time, type, message);
 		this.startCountDown();
 	}
 
-	setTimeForLongBreak() {
-		this.setState({
-			time: this.timeFor.longBreak
-		});
-
+	takeLongBreak() {
+		const { time, alert } = this.constants.longBreak;
+		const { type, message } = alert;
+		
+		this.setTimeAndMessage(time, type, message);
 		this.startCountDown();
 	}
 
-	formatTime(input) {
-		const m = Math.floor(input % 3600 / 60);
-		const s = Math.floor(input % 3600 % 60);
+	formatTime(seconds) {
+		const m = Math.floor(seconds % 3600 / 60);
+		const s = Math.floor(seconds % 3600 % 60);
 
 		return `${m < 10 ? '0' : ''}${m}:${s < 10 ? '0' : ''}${s}`;
 	}
 
 	startCountDown() {
-		clearInterval(this.interval);
+		this.stopOrResetCountDown();
 
 		this.interval = setInterval(() => {
-			this.setState(function(state) {
-				let newState = Object.assign({}, state);
-				newState.time = state.time - 1;
+			if(this.state.time === 0) {
+				const { time, alert } = this.constants.buzz;
+				const { type, message } = alert;
+				
+				this.setTimeAndMessage(time, type, message);
+				this.stopOrResetCountDown();
+			}
+			else {
+				this.setState(function(state) {
+					let newState = Object.assign({}, state);
+					newState.time = state.time - 1;
 
-				return newState;
-			});
+					return newState;
+				});	
+			}
 		}, 1000);
 	}
 
+	stopOrResetCountDown() {
+		clearInterval(this.interval);
+	}
+
 	render() {
-		const { time } = this.state;
+		const { time, alert } = this.state;
+		const { type, message } = alert;
 
 		return (
 			<div className='Pomodoro'>
+
+				<div className={`alert ${type}`}>
+					{message}
+				</div>
 
 				<div className='timer'>
 					{this.formatTime(time)}
 				</div>
 
 				<div className='types'>
-					<button className='start' onClick={this.setTimeForWork}>Start Working</button>
-					<button className='short' onClick={this.setTimeForShortBreak}>Short Break</button>
-					<button className='long' onClick={this.setTimeForLongBreak}>Long Break</button>
+					<button className='start' onClick={this.startWork}>Start Working</button>
+					<button className='short' onClick={this.takeShortBreak}>Short Break</button>
+					<button className='long' onClick={this.takeLongBreak}>Long Break</button>
 				</div>
 			</div>
 		);
